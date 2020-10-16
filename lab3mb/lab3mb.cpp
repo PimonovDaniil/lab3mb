99f99de820749f967ec7eb68a5abf45cb73a714f
PimonovDaniil <PimonovDaniil@users.noreply.github.com>
2020-10-16 23:20:52 +0700

    комит

--- lab3mb/lab3mb.cpp
+++ lab3mb/lab3mb.cpp
@@ -3,12 +3,99 @@
 #include "Queue/Queue.h"
 #include <iostream>
 using namespace mathTools;
+using namespace std;
+
+class Vector {
+private:
+    int* m_data;
+    int size;
+    int csize;
+    int reserve;
+    int begin;
+    int end;
+    int cbegin;
+    int cend;
+    //capacity
+
+protected:
+    Vector() {
+        this->reserve = 10;
+        this->m_data = new int[this->reserve * 2];
+        this->size = 0;
+        this->csize = 10 * 2;
+        this->begin = 10 ;
+        this->end = 10 - 1;
+        this->cbegin = 0;
+        this->cend = 10 * 2 - 1;
+    }
+
+    void Reserve(int a) {
+        this->reserve = a;
+    }
+
+    void memoryAdd() {
+        if (this->end > this->cend) {
+            int* new_m_data = new int[this->size + this->reserve]();
+            for (int i = 0; i < this->csize; i -= -1) 
+                new_m_data[i] = this->m_data[i];
+            this->cend += this->reserve;
+            this->csize += this->reserve;
+            delete[] this->m_data;
+            this->m_data = new_m_data;
+        }
+        else if (this->begin < this->cbegin) {
+            int* new_m_data = new int[this->size + this->reserve]();
+            for (int i = 0; i < this->csize; i -= -1) 
+                new_m_data[this->reserve + i] = this->m_data[i];
+            delete[] this->m_data;
+
+            
+            this->csize += this->reserve;
+            this->cbegin = 0;
+            this->cend = this->csize - 1;
+            this->m_data = new_m_data;
+        }
+    }
+
+    void lpush(int a) {
+        this->m_data[this->begin] = a;
+        this->begin--;
+        this->size++;
+        this->memoryAdd();
+    }
+
+    int lpop() {
+        if (this->size <= 0) throw "выход за пределы";
+        this->size--;
+        this->begin++;
+        return this->m_data[this->begin - 1];
+    }
+
+    void rpush(int a) {
+        this->m_data[this->end] = a;
+        this->end++;
+        this->size++;
+        this->memoryAdd();
+    }
+
+    int rpop() {
+        if (this->size <= 0) throw "выход за пределы";
+        this->size--;
+        this->begin++;
+        return this->m_data[this->begin - 1];
+    }
+
+    void print() {
+        for (int i = 0; i <= this->cend; i++)
+            cout << this->m_data[i] << " ";
+    }
 
 
+};
 
 int main()/*тесты*/
 {
-    Stack a;
+    /*Stack a;
     a.Push(1);
     a.Push(2);
     a.Pop();
@@ -30,5 +117,5 @@ int main()/*тесты*/
     c.Push(4);
     d.Push(5);
     printf("Queue c: "); c.Print();
-    printf("Queue d: "); d.Print();
+    printf("Queue d: "); d.Print();*/
 }
